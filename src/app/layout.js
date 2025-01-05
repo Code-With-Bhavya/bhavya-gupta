81e9b0403bec806715922266d3002e5d6f3cb5df
Bhavya <guptabhavya1402@gmail.com>
2025-01-05 15:29:06 +0530

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Bhavya Gupta || Portfolio",
+  description: "Bhavya Gupta's Portfolio. I am a Full Stack Web Developer. I am a enthusiast in web development and I love to work on new projects.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
