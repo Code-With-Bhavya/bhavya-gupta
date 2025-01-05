f125761e3ac230fd39f2d68428d3ec2c16a75f9d
Bhavya <guptabhavya1402@gmail.com>
2025-01-05 17:49:32 +0530

    fsd

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,33 +2,28 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
-import reactlogo from '../../public/reactlogo.svg';
-import nextjslogo from '../../public/nextjslogo.svg';
-import csslogo from '../../public/csslogo.svg';
-import htmllogo from '../../public/htmllogo.svg';
 
 export default function Skills() {
 
     const [currenttab, setCurrenttab] = useState(1)
 
     const frontendcards = [
-        { src: reactlogo, title: 'ReactJS', level: 'Intermediate', textcolor: 'blue' },
-        { src: nextjslogo, title: 'NextJS', level: 'Intermediate', textcolor: 'black' },
-        { src: csslogo, title: 'CSS', level: 'Intermediate', textcolor: 'blue' },
-        { src: htmllogo, title: 'Html', level: 'Intermediate', textcolor: 'orange' }
-    ]
-    const backendcards = [
-        { src: reactlogo, title: 'ReactJS', level: 'Intermediate', textcolor: 'blue' },
-        { src: nextjslogo, title: 'NextJS', level: 'Intermediate', textcolor: 'black' },
-        { src: csslogo, title: 'CSS', level: 'Intermediate', textcolor: 'blue' },
-        { src: htmllogo, title: 'Html', level: 'Intermediate', textcolor: 'orange' }
-    ]
-    const librariescards = [
-        { src: reactlogo, title: 'ReactJS', level: 'Intermediate', textcolor: 'blue' },
-        { src: nextjslogo, title: 'NextJS', level: 'Intermediate', textcolor: 'black' },
-        { src: csslogo, title: 'CSS', level: 'Intermediate', textcolor: 'blue' },
-        { src: htmllogo, title: 'Html', level: 'Intermediate', textcolor: 'orange' }
-    ]
+        { src: "/reactlogo.svg", title: "ReactJS", level: "Intermediate", textcolor: "text-blue-500" },
+        { src: "/nextjslogo.svg", title: "NextJS", level: "Intermediate", textcolor: "text-black" },
+        { src: "/csslogo.svg", title: "CSS", level: "Intermediate", textcolor: "text-blue-500" },
+        { src: "/htmllogo.svg", title: "Html", level: "Intermediate", textcolor: "text-orange-500" },
+      ];
+      const backendcards = [
+        { src: "/pythonlogo.svg", title: "Python", level: "Intermediate", textcolor: "text-yellow-500" },
+        { src: "/nodejslogo.svg", title: "NodeJS", level: "Intermediate", textcolor: "text-green-500" },
+      ];
+      const librariescards = [
+        { src: "/tailwindlogo.svg", title: "TailwindCSS", level: "Intermediate", textcolor: "text-blue-500" },
+        { src: "/socketiologo.svg", title: "Socket.io", level: "Intermediate", textcolor: "text-black" },
+        { src: "/githublogo.svg", title: "Github", level: "Intermediate", textcolor: "text-black" },
+      ];
+
+      const cards = currenttab === 1 ? frontendcards : currenttab === 2 ? backendcards : librariescards;
 
     return (
         <div className="w-full  flex flex-col gap-5">
@@ -48,13 +43,13 @@ export default function Skills() {
                 >Libraries</div>
             </div>
 
-            <div className='flex gap-3'> {/* CARDS */}
-                {(currenttab == 0 ? frontendcards : currenttab == 1 ? backendcards : librariescards).map((index, data) => {
+            <div className='flex justify-between'> {/* CARDS */}
+                {cards.map((data, index) => {
                     return (
-                        <div key={index} className='bg-[#3D3F46] rounded-xl'>
-                            <Image src={data.src} width={100} height={100} alt={data.title} />
+                        <div key={index} className='bg-[#3D3F46] rounded-xl flex flex-col justify-center items-center'>
+                            <Image src={data.src} width={50} height={10} alt={data.title} />
                             <h2>{data.title}</h2>
-                            <h4>{data.level}</h4>
+                            <h4 className={`${data.textcolor}`}>{data.level}</h4>
                         </div>
                     )
                 })}
