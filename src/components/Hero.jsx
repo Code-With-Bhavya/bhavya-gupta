3b120c0df8757bad664d27d6c0c3af822f74f134
Rupam Das <150364981+ItzFalco07@users.noreply.github.com>
2025-01-05 11:33:16 +0000

    v1.5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,16 @@
 
 export default function Hero () {
     return (
-        <div id="hero" className="w-full bg-red-400">
-            Hero
+        <div id="hero" className="relative w-full h-screen flex">
+            <div className="relative top-[28vh]">
+                <p className="text-[2em] font-medium"> <span className="text-[#FD6F00]">Hello</span> , I'm</p>
+                <h1 className=" font-bold mt-[-10px] text-[4em]">Bhavya gupta</h1>
+                <p className="opacity-[0.8] mt- w-[400px]">Fullstack Web Developer, ui/ux designer and a enthusiast freelancer. For making your online presence reach me today!</p>
+
+                <button className="py-2 px-6 font-medium bg-[#FD6F00] rounded-[2px] mt-8">Hire Me</button>
+            </div>
+
+            <div></div>
         </div>
     )
 }
