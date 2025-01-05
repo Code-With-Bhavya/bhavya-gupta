bfa8f0f0f1bf8b6189c37172abbfdc7c8fb51670
Rupam Das <150364981+ItzFalco07@users.noreply.github.com>
2025-01-05 11:05:05 +0000

    v2.4 - added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import Link from 'next/link'
 
 export default function Navbar () {
     return (
-        <nav className="w-full h-[50px] flex justify-between items-center">
+        <nav className="w-full relative h-[50px] flex justify-between items-center">
             {/* Logo */}
-            <h1 className='font-semibold text-[1.2em] text-[#FD6F00]'><span className='text-white'>&lt;</span> CodeWithBhavya <span className='text-white'>/&gt;</span></h1>
+            <h1 className='font-semibold text-[1.3em] text-[#FD6F00]'><span className='text-white'>&lt;</span> CodeWithBhavya <span className='text-white'>/&gt;</span></h1>
 
             {/* Links */}
-            <div className='flex gap-6'>
+            <div className='flex gap-6 h-full flex items-center py-[7px]'>
                 <Link href="#">Skills</Link>
                 <Link href="#">Projects</Link>
                 <Link href="#">About Us</Link>
                 <Link href="#">Services</Link>
                 <Link href="#">Contact Us</Link>
 
-                <button>Hire Me</button>
+                <button className='bg-[#FD6F00] font-medium font-medium h-full px-[20px] rounded-[2px]'> Hire Me</button>
             </div>
         </nav>
     )
