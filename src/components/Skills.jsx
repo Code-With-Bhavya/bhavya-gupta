1c6f51eddfc88ffc0378e6e126a08128dd969490
Bhavya <guptabhavya1402@gmail.com>
2025-01-05 17:21:36 +0530

    refactor: update logo imports and adjust tab styles in Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,14 +2,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
-import reactlogo from '@/assets/reactlogo';
-import nextjslogo from '@/assets/nextjslogo';
-import csslogo from '@/assets/csslogo';
-import htmllogo from '@/assets/htmllogo';
+import reactlogo from '../../public/reactlogo.svg';
+import nextjslogo from '../../public/nextjslogo.svg';
+import csslogo from '../../public/csslogo.svg';
+import htmllogo from '../../public/htmllogo.svg';
 
 export default function Skills() {
 
-    const [currenttab, setCurrenttab] = useState(0)
+    const [currenttab, setCurrenttab] = useState(1)
 
     const frontendcards = [
         { src: reactlogo, title: 'ReactJS', level: 'Intermediate', textcolor: 'blue' },
@@ -35,7 +35,7 @@ export default function Skills() {
             Skills
 
             <div className="flex gap-3 "> {/* TABS */}
-                <div className={`px-2 py-1 ${currenttab === 1 ? 'bg-[#FD6F00]' : 'bg-[#0E1016]'} rounded-l-10 rounded-r-none `}
+                <div className={`px-2 py-1 ${currenttab === 1 ? 'bg-[#FD6F00]' : 'bg-[#0E1016]'} rounded-l-2xl rounded-r-none `}
                     onClick={() => { setCurrenttab(1) }}
                 >Frontend</div>
 
@@ -43,20 +43,20 @@ export default function Skills() {
                     onClick={() => { setCurrenttab(2) }}
                 >Backend</div>
 
-                <div className={`px-2 py-1 ${currenttab === 3 ? 'bg-[#FD6F00]' : 'bg-[#0E1016]'} rounded-l-none rounded-r-10 `}
+                <div className={`px-2 py-1 ${currenttab === 3 ? 'bg-[#FD6F00]' : 'bg-[#0E1016]'} rounded-l-none rounded-r-2xl `}
                     onClick={() => { setCurrenttab(3) }}
                 >Libraries</div>
             </div>
 
-            <div> {/* CARDS */}
+            <div className='flex gap-3'> {/* CARDS */}
                 {(currenttab == 0 ? frontendcards : currenttab == 1 ? backendcards : librariescards).map((index, data) => {
-                    <div key={index} className='bg-[#3D3F46] rounded-xl'>
-                        <div className='cardslinearbg'>
-                            <Image src={data.src} width = {100} height= {100} />
+                    return (
+                        <div key={index} className='bg-[#3D3F46] rounded-xl'>
+                            <Image src={data.src} width={100} height={100} alt={data.title} />
                             <h2>{data.title}</h2>
                             <h4>{data.level}</h4>
                         </div>
-                    </div>
+                    )
                 })}
             </div>
 
